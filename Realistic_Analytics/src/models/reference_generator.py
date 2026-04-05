7988b85a379655ab8ede224c6ff37b3998593170
rew35860 <njengjirapas@ethz.ch>
2026-04-05 23:04:00 +0200

    Fixed return values of model prediction

--- Realistic_Analytics/src/models/reference_generator.py
+++ Realistic_Analytics/src/models/reference_generator.py
@@ -10,4 +10,4 @@ class SinusoidalReference:
         # Convert phase into desired position and velocity
         x_ref = self.A * torch.sin(phi)
         v_ref = self.A * torch.cos(phi) * phi_dot
-        return x_ref, v_ref, x_ref.unsqueeze(0)
+        return x_ref, v_ref, x_ref.unsqueeze(0), None
