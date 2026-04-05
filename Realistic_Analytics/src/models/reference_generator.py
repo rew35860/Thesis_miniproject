d929090112338345f315571303bd58009e86292f
rew35860 <njengjirapas@ethz.ch>
2026-04-05 22:07:28 +0200

    Added more mlp trained models

--- Realistic_Analytics/src/models/reference_generator.py
+++ Realistic_Analytics/src/models/reference_generator.py
@@ -6,8 +6,8 @@ class SinusoidalReference:
         # Amplitude of the sinusoidal reference trajectory
         self.A = A
 
-    def get_reference(self, phi, phi_dot):
+    def get_reference(self, x, v, phi, phi_dot):
         # Convert phase into desired position and velocity
         x_ref = self.A * torch.sin(phi)
         v_ref = self.A * torch.cos(phi) * phi_dot
-        return x_ref, v_ref
+        return x_ref, v_ref, x_ref.unsqueeze(0)
